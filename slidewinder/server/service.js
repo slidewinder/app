adf8ec1dc84221947ad500e8e7f43353e02a8293
Richard Smith-Unna <richardsmith404@gmail.com>
2016-01-28 15:16:03 +0000

    Login workflow improvements

---
+++ slidewinder/server/service.js
@@ -0,0 +1,8 @@
+ServiceConfiguration.configurations.remove({ service: 'google' })
+
+ServiceConfiguration.configurations.insert({
+  service: 'google',
+  clientId: '508719023243-dq6395hc6hdfvghaq0qcdcml4odu1qup.apps.googleusercontent.com',
+  secret: 'o7jnKeLx6Gt1rjaLp5SjGnd2',
+  forceApprovalPrompt: false
+})

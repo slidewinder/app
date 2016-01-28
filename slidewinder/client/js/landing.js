2c9a46a92cbdad42b768d3a715103bad008ba94b
Richard Smith-Unna <richardsmith404@gmail.com>
2016-01-28 11:40:15 +0000

    Landing and home page separation

--- slidewinder/client/js/landing.js
+++ slidewinder/client/js/landing.js
@@ -1,4 +1,4 @@
-Template.home.events({
+Template.landing.events({
   "click #get-started-btn": function(event) {
     $('#get-started').animate({ height: 0 }, function() {
       $('#home-wrapper').animate({ 'margin-top': '5%' });
--- slidewinder/client/js/router.js
+++ slidewinder/client/js/router.js
@@ -1,5 +1,15 @@
 FlowRouter.route('/', {
   action: function() {
+    if (Meteor.userId()) {
+      FlowRouter.go('/home');
+    } else {
+      BlazeLayout.render("index", {content: "landing"});
+    }
+  }
+});
+
+FlowRouter.route('/home', {
+  action: function() {
     BlazeLayout.render("index", {content: "home"});
   }
 });

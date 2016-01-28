481212cba4786259cc6b40d4c08cd86cdf1e6b6e
Richard Smith-Unna <richardsmith404@gmail.com>
2016-01-28 13:37:20 +0000

    Setup for major routes and working login/logout

--- slidewinder/client/js/router.js
+++ slidewinder/client/js/router.js
@@ -1,6 +1,6 @@
 FlowRouter.route('/', {
   action: function() {
-    if (Meteor.userId()) {
+    if (Meteor.user() || Meteor.loggingIn()) {
       FlowRouter.go('/home');
     } else {
       BlazeLayout.render("index", {content: "landing"});
@@ -11,5 +11,51 @@ FlowRouter.route('/', {
 FlowRouter.route('/home', {
   action: function() {
     BlazeLayout.render("index", {content: "home"});
+  }
+});
+
+var slidesSection = FlowRouter.group({
+  prefix: '/slides'
+});
+
+slidesSection.route('/create', {
+  action: function() {
+    BlazeLayout.render("tiered", {content: "create_slide"});
+  }
+});
+
+var decksSection = FlowRouter.group({
+  prefix: '/decks'
+});
+
+decksSection.route('/create', {
+  action: function() {
+    BlazeLayout.render("tiered", {content: "create_deck"});
+  }
+});
+
+decksSection.route('/present', {
+  action: function() {
+    BlazeLayout.render("tiered", {content: "present_deck"});
+  }
+});
+
+FlowRouter.route('/library', {
+  action: function() {
+    BlazeLayout.render("tiered", {content: "library"});
+  }
+});
+
+FlowRouter.route('/help', {
+  action: function() {
+    BlazeLayout.render("tiered", {content: "help"});
+  }
+});
+
+FlowRouter.route('/logout', {
+  action: function() {
+    Meteor.logout(function(err) {
+      FlowRouter.go('/');
+    });
   }
 });
